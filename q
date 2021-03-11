ffe00a1cff10cbdb879f52d4cb33266b20270dd6
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-11 17:07:12 +0200

    adding some helping comments about gates and policies  in web.php file

---
+++ q
@@ -0,0 +1,12 @@
+  auth[m
+  events[m
+* [32mfacades[m
+  gates_and_policies[m
+  main[m
+  mergable[m
+  notification[m
+  online_payment[m
+  queue_notification[m
+  rt-notifications[m
+  sending_emails[m
+  sending_emails_gmail[m
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
+// steps for authorization using gates and policies 
+// 1- define gate [define the logic which define hoe the user is autheroized ]. How?
+//                 1-via closure
+//                 2-using policies "think of 'gates' and 'policies' like a 'routes' and 'controllers'"  how?
+//                   I-create policy  II-define logic for actions inside it  III- rgiester it in 'AuthServiceProvider'
+
+// 2- use the gate within  ['views','user model', 'controller','routes','middleware']
+
+
+
+
+
 
 // gates
 Route::get('/subs',function(){
